a2a61d490b4fbf05905813371cea053592975464
tritm97 <minhtripro97@gmail.com>
2023-08-13 23:11:19 +0700

    làm giao diện và validation registerpage

--- js/register.js
+++ js/register.js
@@ -21,7 +21,15 @@ let dangKy = () => {
     console.log(khachHang);
 
     let valid = true;
-    valid = valid && kiemTraDuLieuRong(arrIdInput, arrNotiInput, khachHang, arrLabelInput);
+    let valueEmail = document.getElementById('email').value;
+    let valueTen = document.getElementById('name').value;
+    let valuePassword = document.getElementById('password').value;
+    let valueRePassword = document.getElementById('re_password').value;
+    let valueGender = document.getElementById('gender').value;
+
+    valid = valid && kiemTraDuLieuRong(arrIdInput, arrNotiInput, khachHang, arrLabelInput) && kiemTraTen(valueTen, 'tbName') && kiemTraEmail(valueEmail, 'tbEmail') && kiemTraPassword(valuePassword, 'tbPassword') && kiemTraRePassword(valueRePassword, valuePassword, 'tbRePassword') && kiemTraGender(valueGender, 'tbGender') && kiemTraSDT('tbPhone');
+    // console.log(valid);
+    // console.log(typeof valid);
 
     if (valid) {
         alert('đăng ký thành công');
