5618b64d1441cc70ca1be093cc3158c066343f3d
tritm97 <minhtripro97@gmail.com>
2023-08-14 12:14:24 +0700

    update register page & register js

--- js/register.js
+++ js/register.js
@@ -1,39 +1,129 @@
-let arrIdInput = [
-    'name', 'email', 'password', 're_password', 'gender', 'phone'
+const MALE_ENUM = "male";
+const FEMALE_ENUM = "female";
+
+const GENDER_MAP = {
+  [MALE_ENUM]: true,
+  [FEMALE_ENUM]: false,
+};
+
+const arrIdInput = [
+  "name",
+  "email",
+  "password",
+  "re_password",
+  "gender",
+  "phone",
 ];
 
-let arrNotiInput = [
-    'tbName', 'tbEmail', 'tbPassword', 'tbRePassword', 'tbGender', 'tbPhone'
+const arrNotiInput = [
+  "tbName",
+  "tbEmail",
+  "tbPassword",
+  "tbRePassword",
+  "tbGender",
+  "tbPhone",
 ];
 
-let arrLabelInput = [
-    'lbName', 'lbEmail', 'lbPassword', 'lbRePassword', 'lbGender', 'lbPhone'
+const arrLabelInput = [
+  "lbName",
+  "lbEmail",
+  "lbPassword",
+  "lbRePassword",
+  "lbGender",
+  "lbPhone",
 ];
 
-let dangKy = () => {
-    event.preventDefault();
+const REGISTER_URL = "https://shop.cyberlearn.vn/api/Users/signup";
 
-    let khachHang = new KhachHang();
-    for (let i = 0; i < arrIdInput.length; i++) {
-        let value = document.getElementById(arrIdInput[i]).value;
-        khachHang[arrIdInput[i]] = value;
-    }
-    console.log(khachHang);
+const onSuccess = (response) => {
+  const { statusCode, message } = response?.data;
+  if (statusCode !== 200) {
+    Toastify({
+      text: message,
+      className: "error", // update css classname
+    }).showToast();
+  } else {
+    Toastify({
+      text: `Đăng ký thành công!`,
+      className: "success",
+    }).showToast();
+    document.getElementById("signUpForm").reset();
+    document.getElementById("btnGoHome").style.display = "inline-block";
+    document.getElementById("btnSignUp").style.display = "none";
+  }
+};
 
-    let valid = true;
-    let valueEmail = document.getElementById('email').value;
-    let valueTen = document.getElementById('name').value;
-    let valuePassword = document.getElementById('password').value;
-    let valueRePassword = document.getElementById('re_password').value;
-    let valueGender = document.getElementById('gender').value;
+const onError = (error) => {
+  console.log(error);
+  const message = error.response.data.message || "Có lỗi xảy ra!";
+  Toastify({
+    text: message,
+    className: "error",
+  }).showToast();
+};
 
-    valid = valid && kiemTraDuLieuRong(arrIdInput, arrNotiInput, khachHang, arrLabelInput) && kiemTraTen(valueTen, 'tbName') && kiemTraEmail(valueEmail, 'tbEmail') && kiemTraPassword(valuePassword, 'tbPassword') && kiemTraRePassword(valueRePassword, valuePassword, 'tbRePassword') && kiemTraGender(valueGender, 'tbGender') && kiemTraSDT('tbPhone');
-    // console.log(valid);
-    // console.log(typeof valid);
+const getCustomer = () => {
+  let customer = new KhachHang();
+  for (let i = 0; i < arrIdInput.length; i++) {
+    const value = document.getElementById(arrIdInput[i]).value;
+    customer[arrIdInput[i]] = value;
+  }
+  return customer;
+};
 
-    if (valid) {
-        alert('đăng ký thành công');
-        document.getElementById('signUpForm').reset();
-    } 
-}
+const getValues = (keys) => {
+  const data = {};
+  keys.forEach((key) => {
+    const valueByKey = document.getElementById(key).value;
+    data[key] = valueByKey;
+  });
+
+  return data;
+};
+
+const isValid = (rawData, customer) => {
+  return (
+    kiemTraDuLieuRong(arrIdInput, arrNotiInput, customer, arrLabelInput) &&
+    kiemTraTen(rawData.name, "tbName") &&
+    kiemTraEmail(rawData.email, "tbEmail") &&
+    kiemTraPassword(rawData.password, "tbPassword") &&
+    kiemTraRePassword(rawData.password, rawData.re_password, "tbRePassword") &&
+    kiemTraGender(rawData.gender, "tbGender") &&
+    kiemTraSDT("tbPhone")
+  );
+};
+
+const onRegister = () => {
+  event.preventDefault();
+
+  const customer = getCustomer();
+
+  // Get form data
+  const rawData = getValues([
+    "email",
+    "name",
+    "password",
+    "re_password",
+    "gender",
+    "phone",
+  ]);
+
+  // validation
+  if (isValid(rawData, customer)) {
+    // Body Data
+    const bodyData = {
+      email: rawData.email,
+      password: rawData.password,
+      name: rawData.name,
+      gender: GENDER_MAP[rawData.gender],
+      phone: rawData.phone,
+    };
+    // make request
+    axios.post(REGISTER_URL, bodyData).then(onSuccess).catch(onError);
+  }
+};
+
+const goHomePage = () => {
+  window.location.href = "./../index.html";
+};
 
--- validation/validation.js
+++ validation/validation.js
@@ -1,8 +1,8 @@
-let kiemTraDuLieuRong = (arrIdInput, arrNotiInput, khachHang, arrLabelInput) => {
+let kiemTraDuLieuRong = (arrIdInput, arrNotiInput, customer, arrLabelInput) => {
     let valid = true;
     for (let z = 0; z < arrIdInput.length; z++) {
         // check xem có dữ liệu trong thuộc tính hay không
-        if (khachHang[arrIdInput[z]] == '') {
+        if (customer[arrIdInput[z]] == '') {
             valid = valid && false;
             document.getElementById(arrNotiInput[z]).innerHTML = 'Vui lòng nhập dữ liệu';
             document.getElementById(arrLabelInput[z]).style.color = 'red';
